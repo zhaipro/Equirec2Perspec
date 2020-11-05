f62bcb5a1a781c34e964973ebb7c0166e8f759d3
lixin <lixin@moviebook.cn>
2020-11-05 10:46:04 +0800

    面向函数式编程

--- Equirec2Perspec.py
+++ Equirec2Perspec.py
@@ -1,86 +1,59 @@
 import os
 import sys
+
 import cv2
 import numpy as np
 
-class Equirectangular:
-    def __init__(self, img_name):
-        self._img = cv2.imread(img_name, cv2.IMREAD_COLOR)
-        [self._height, self._width, _] = self._img.shape
-        #cp = self._img.copy()  
-        #w = self._width
-        #self._img[:, :w/8, :] = cp[:, 7*w/8:, :]
-        #self._img[:, w/8:, :] = cp[:, :7*w/8, :]
-    
 
-    def GetPerspective(self, FOV, THETA, PHI, height, width, RADIUS = 128):
-        #
-        # THETA is left/right angle, PHI is up/down angle, both in degree
-        #
+def get_perspective(im, FOV, THETA, PHI, height, width):
+    #
+    # THETA is left/right angle, PHI is up/down angle, both in degree
+    #
+    equ_h, equ_w, _ = im.shape
+    equ_cx = (equ_w - 1) / 2.0
+    equ_cy = (equ_h - 1) / 2.0
 
-        equ_h = self._height
-        equ_w = self._width
-        equ_cx = (equ_w - 1) / 2.0
-        equ_cy = (equ_h - 1) / 2.0
+    wFOV = FOV
+    w_len = np.tan(np.radians(wFOV / 2.0))      # 默认半径为1
+    h_len = height / width * w_len
 
-        wFOV = FOV
-        hFOV = float(height) / width * wFOV
+    x_map = np.linspace(-w_len, w_len, width).reshape((1, -1))
+    y_map = np.linspace(-h_len, h_len, height).reshape((-1, 1))
+    z_map = 1                                   # 默认半径为1
+    D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
+    xyz = np.zeros([height, width, 3], np.float)
+    xyz[:, :, 0] = x_map / D
+    xyz[:, :, 1] = y_map / D
+    xyz[:, :, 2] = z_map / D
 
-        c_x = (width - 1) / 2.0
-        c_y = (height - 1) / 2.0
+    x_axis = np.array([1.0, 0.0, 0.0], np.float32)
+    y_axis = np.array([0.0, 1.0, 0.0], np.float32)
+    R1, _ = cv2.Rodrigues(y_axis * np.radians(THETA))
+    R2, _ = cv2.Rodrigues(np.dot(R1, x_axis) * np.radians(PHI))
+    R = R2 @ R1
 
-        wangle = (180 - wFOV) / 2.0
-        w_len = 2 * RADIUS * np.sin(np.radians(wFOV / 2.0)) / np.sin(np.radians(wangle))
-        w_interval = w_len / (width - 1)
+    xyz = xyz @ R.T
+    lat = np.arcsin(xyz[..., 1])
+    lon = np.arctan2(xyz[..., 0], xyz[..., 2])
 
-        hangle = (180 - hFOV) / 2.0
-        h_len = 2 * RADIUS * np.sin(np.radians(hFOV / 2.0)) / np.sin(np.radians(hangle))
-        h_interval = h_len / (height - 1)
-        x_map = np.zeros([height, width], np.float32) + RADIUS
-        y_map = np.tile((np.arange(0, width) - c_x) * w_interval, [height, 1])
-        z_map = -np.tile((np.arange(0, height) - c_y) * h_interval, [width, 1]).T
-        D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
-        xyz = np.zeros([height, width, 3], np.float)
-        xyz[:, :, 0] = (RADIUS / D * x_map)[:, :]
-        xyz[:, :, 1] = (RADIUS / D * y_map)[:, :]
-        xyz[:, :, 2] = (RADIUS / D * z_map)[:, :]
-        
-        y_axis = np.array([0.0, 1.0, 0.0], np.float32)
-        z_axis = np.array([0.0, 0.0, 1.0], np.float32)
-        [R1, _] = cv2.Rodrigues(z_axis * np.radians(THETA))
-        [R2, _] = cv2.Rodrigues(np.dot(R1, y_axis) * np.radians(-PHI))
+    lon = lon / np.pi * 180
+    lat = lat / np.pi * 180
+    lon = lon / 180 * equ_cx + equ_cx
+    lat = lat / 90 * equ_cy + equ_cy
 
-        xyz = xyz.reshape([height * width, 3]).T
-        xyz = np.dot(R1, xyz)
-        xyz = np.dot(R2, xyz).T
-        lat = np.arcsin(xyz[:, 2] / RADIUS)
-        lon = np.zeros([height * width], np.float)
-        theta = np.arctan(xyz[:, 1] / xyz[:, 0])
-        idx1 = xyz[:, 0] > 0
-        idx2 = xyz[:, 1] > 0
-
-        idx3 = ((1 - idx1) * idx2).astype(np.bool)
-        idx4 = ((1 - idx1) * (1 - idx2)).astype(np.bool)
-        
-        lon[idx1] = theta[idx1]
-        lon[idx3] = theta[idx3] + np.pi
-        lon[idx4] = theta[idx4] - np.pi
-
-        lon = lon.reshape([height, width]) / np.pi * 180
-        lat = -lat.reshape([height, width]) / np.pi * 180
-        lon = lon / 180 * equ_cx + equ_cx
-        lat = lat / 90 * equ_cy + equ_cy
-        #for x in range(width):
-        #    for y in range(height):
-        #        cv2.circle(self._img, (int(lon[y, x]), int(lat[y, x])), 1, (0, 255, 0))
-        #return self._img 
-    
-        persp = cv2.remap(self._img, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP)
-        return persp
-        
+    persp = cv2.remap(im, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP)
+    return persp
 
 
-
-
-
-
+if __name__ == '__main__':
+    import time
+    frame = cv2.imread('src/image.jpg')
+    n = 100
+    s = time.time()
+    for i in range(n):
+        result = get_perspective(frame, 100, i % 360, 0, 720, 1080)
+        cv2.imshow('a', result)
+        if cv2.waitKey(1) == ord(' '):
+            break
+    print(n / (time.time() - s))
+    frame = get_perspective(frame, 60, -200, 30, 720, 1080)
